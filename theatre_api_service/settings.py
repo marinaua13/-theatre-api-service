5bf8ec287b8bee900d9ec5115b5aa4f17edc6a6e
Marina <marina.ua13@gmail.com>
2024-06-10 13:14:25 +0300

    add media to setting.py

--- theatre_api_service/settings.py
+++ theatre_api_service/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "catalog",
-    "user"
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "/files/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
