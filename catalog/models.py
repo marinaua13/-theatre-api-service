979237e517305b0f5bad50e6da9fb3fa9d3c5fd3
Marina <marina.ua13@gmail.com>
2024-06-13 12:17:00 +0300

    correct models.py

--- catalog/models.py
+++ catalog/models.py
@@ -31,7 +31,7 @@ def play_image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("uploads/movies/", filename)
+    return os.path.join("uploads/plays/", filename)
 
 
 class Play(models.Model):
